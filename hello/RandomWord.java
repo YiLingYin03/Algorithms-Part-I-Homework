8a7fe3a6b7ed3ebdfe3a1ed4b929161fe8a1da25
YINYL <yinyiling_ray@163.com>
2023-08-10 23:34:29 +0800

    doc: completed homework 01.

--- hello/RandomWord.java
+++ hello/RandomWord.java
@@ -1,8 +1,19 @@
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
 
     public static void main(String[] args) {
-        StdOut.print("hello");
+        int i = 0;
+        String winner = "";
+        while (!StdIn.isEmpty()) {
+            i++;
+            String word = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / i)) {
+                winner = word;
+            }
+        }
+        StdOut.println(winner);
     }
 }
