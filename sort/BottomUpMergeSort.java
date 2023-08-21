42217d39e102f3beecb5be6fdfac29c19efdea3c
YINYL <yinyiling_ray@163.com>
2023-08-21 22:06:52 +0800

    feat: BottomUpMergeSort impletementation.

---
+++ sort/BottomUpMergeSort.java
@@ -0,0 +1,63 @@
+/**
+ * The Bottom-up Merge Sort algorithm is efficient
+ * and has the same time complexity as the classic Merge Sort (O(n log n)).
+ * */
+
+import java.util.Arrays;
+
+public class BottomUpMergeSort {
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            rightArray[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k++] = leftArray[i++];
+            } else {
+                arr[k++] = rightArray[j++];
+            }
+        }
+
+        while (i < n1) {
+            arr[k++] = leftArray[i++];
+        }
+        while (j < n2) {
+            arr[k++] = rightArray[j++];
+        }
+    }
+
+    public static void bottomUpMergeSort(int[] arr) {
+        int n = arr.length;
+
+        for (int size = 1; size < n; size *= 2) {
+            for (int left = 0; left < n - size; left += 2 * size) {
+                int mid = left + size - 1;
+                int right = Math.min(left + 2 * size - 1, n - 1);
+                merge(arr, left, mid, right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        int n = arr.length;
+
+        System.out.println("Original array: " + Arrays.toString(arr));
+
+        bottomUpMergeSort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
